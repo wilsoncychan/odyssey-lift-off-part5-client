bf5f7c5d285dd08c84db78cc169c518ff08db27e
WChan <wilson.chan@industrie.co>
2022-06-22 11:10:39 +0800

    update url

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  uri: "https://server-catstronaunts.herokuapp.com/", // change to YOUR own production server
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
